342ae2ecbdc1f624c7828dbe4d776f4a3b15fa5d
chlbri <51156116+chlbri@users.noreply.github.com>
2026-07-31 10:25:47 +0000

    Delete apps/solidjs/.output directory
    Signed-off-by: chlbri <51156116+chlbri@users.noreply.github.com>

--- apps/solidjs/.output/server/_libs/bemedev__sleep.mjs
+++
@@ -1,6 +0,0 @@
-//#region ../../node_modules/@bemedev/sleep/lib/index.js
-var sleep = (milliseconds = 100) => {
-	return new Promise((resolve) => setTimeout(resolve, milliseconds));
-};
-//#endregion
-export { sleep as t };
